6d035dc22df9c0f246ef085ecd0251d657cab9f9
Dominic Shelton <drs73@uclive.ac.nz>
2015-06-10 15:46:53 +1200

    added changing values and long clicks

--- src/gameWindow.c
+++ src/gameWindow.c
@@ -5,19 +5,61 @@
 
 #include "gameWindow.h"
 
+#define TEXT_LEN 9
+#define VALUES 3
+#define LONG_CLICK_DURATION 700
+enum {VALUE_CLICKS = 0, VALUE_CREDS = 1, VALUE_CREDS_RECUR = 2};
+
 static Window *window;
 static TextLayer *text_layer;
+static GColor s_fg, s_bg;
+static int value[2][3] = {{0, 0, 0}, {0, 5, 0}};
+static int selectedValue = 0;
+static TextLayer* valueText[3];
+
+static void change_value(bool total, int amount) {
+    value[0][selectedValue] += amount;
+    // values can't go below 0.
+    if (value[0][selectedValue] < 0) value[0][selectedValue] = 0;
+    // Change the total value if applicable.
+    if (total && selectedValue != VALUE_CREDS) {
+        value[1][selectedValue] += amount;
+        // Total values can't go below 0 either.
+        if (value[1][selectedValue] < 0) value[1][selectedValue] = 0;
+        // If the value goes above the total, do something.
+        if (value[0][selectedValue] > value[1][selectedValue]) {
+            if (selectedValue == VALUE_CREDS_RECUR)
+                value[0][selectedValue] = value[1][selectedValue];
+        }
+    }
+    char displayText[TEXT_LEN] = {'\0'};
+    if (selectedValue == VALUE_CLICKS) {
+        snprintf(displayText, TEXT_LEN, "%d of %d", value[0][selectedValue], value[0][selectedValue]);
+    }
+    else {
+        snprintf(displayText, TEXT_LEN, "%d", value[0][selectedValue]);
+    }
+    text_layer_set_text(valueText[selectedValue], displayText);
+}
 
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-    text_layer_set_text(text_layer, "Select");
+    selectedValue = (selectedValue + 1) % VALUES;
 }
 
 static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
-    text_layer_set_text(text_layer, "Up");
+    change_value(false, 1);
 }
 
 static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-    text_layer_set_text(text_layer, "Down");
+    change_value(false, -1);
+}
+
+static void up_long_handler(ClickRecognizerRef recognizer, void *context) {
+    change_value(true, 1);
+}
+
+static void down_long_handler(ClickRecognizerRef recognizer, void *context) {
+    change_value(true, -1);
 }
 
 static void back_click_handler(ClickRecognizerRef recognizer, void *context) {
@@ -29,15 +71,17 @@ static void back_click_handler(ClickRecognizerRef recognizer, void *context) {
     }
 }
 
-
 static void click_config_provider(void *context) {
     window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
     window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
+    window_long_click_subscribe(BUTTON_ID_UP, LONG_CLICK_DURATION, up_long_handler, NULL);
     window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
+    window_long_click_subscribe(BUTTON_ID_DOWN, LONG_CLICK_DURATION, down_long_handler, NULL);
     window_single_click_subscribe(BUTTON_ID_BACK, back_click_handler);
 }
 
 static void window_load(Window *window) {
+    window_set_background_color(window, s_bg);
     Layer *window_layer = window_get_root_layer(window);
     GRect bounds = layer_get_bounds(window_layer);
 
@@ -58,11 +102,10 @@ void gameWindow_init(GColor bg, GColor fg, int clicks) {
         .load = window_load,
         .unload = window_unload,
     });
-    const bool animated = true;
-#ifdef PBL_COLOR
-    window_set_background_color(window,bg);
-#endif //PBL_COLOR
-    window_stack_push(window, animated);
+
+    s_fg = fg;
+    s_bg = bg;
+    window_stack_push(window, true);
 }
 
 void gameWindow_deinit(void) {
